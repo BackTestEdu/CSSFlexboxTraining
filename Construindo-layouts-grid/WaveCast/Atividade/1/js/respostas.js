d546dfbfdadd33baa0a1bb113577bea0e5aecfd6
Edu Back <backtestedu@gmail.com>
2023-11-14 16:03:09 -0300

    Removido redundancias e deixado a código mais generico

--- Construindo-layouts-grid/WaveCast/Atividade/1/js/respostas.js
+++ Construindo-layouts-grid/WaveCast/Atividade/1/js/respostas.js
@@ -1,21 +1,24 @@
 function resposta(resposta) {
     let descricao = document.getElementsByClassName('descricao')[0];
+    let buttons = document.querySelectorAll('button');
+    const acertou = "Alternativa correta!";
+
+    btnReset();
+
     switch (resposta) {
         case "A":
-            btnReset();
-            document.getElementsByTagName('button')[0].style.color = 'lightgreen';
+            buttons[0].style.color = 'lightgreen';
             document.body.style.display = 'grid';
             document.body.style.gridTemplateAreas = '"header header" "main aside"';
             document.querySelector('.cabecalho').style.gridArea = 'header';
             document.querySelector('.lateral').style.gridArea = 'aside';
             document.querySelector('.principal').style.gridArea = 'main';
             document.querySelector('.descricao').style.color = 'green';
-            descricao.innerHTML = "Alternativa correta!";
-           
+            descricao.innerHTML = acertou;
+
             break;
         case "B":
-            btnReset();
-            document.getElementsByTagName('button')[1].style.color = 'red';
+            buttons('button')[1].style.color = 'red';
             document.body.style.display = 'grid';
             document.body.style.gridTemplateAreas = '"header header" "main aside"';
             document.querySelector('.cabecalho').style.gridArea = 'cabecalho';
@@ -25,22 +28,20 @@ function resposta(resposta) {
             descricao.innerHTML = "Pense mais um pouco! O valor passado para as classes deve sempre ser correspondente aos valores passados na propriedade grid-template-areas.";
             break;
         case "C":
-            btnReset();
-            document.getElementsByTagName('button')[2].style.color = 'red';
+            buttons[2].style.color = 'red';
             document.body.style.display = 'grid';
             document.body.style.gridTemplateAreas = '"aside aside" "main header"';
-            document.querySelector('.cabecalho').style.display ='grid';
+            document.querySelector('.cabecalho').style.display = 'grid';
             document.querySelector('.cabecalho').style.gridArea = 'header';
-            document.querySelector('.lateral').style.display ='grid';
+            document.querySelector('.lateral').style.display = 'grid';
             document.querySelector('.lateral').style.gridArea = 'aside';
-            document.querySelector('.lateral').style.display ='grid';
+            document.querySelector('.lateral').style.display = 'grid';
             document.querySelector('.principal').style.gridArea = 'main';
             document.querySelector('.descricao').style.color = 'red';
             descricao.innerHTML = "Pense mais um pouco! O valor passado para as classes deve sempre ser correspondente aos valores passados na propriedade grid-template-areas.";
             break;
         case "D":
-            btnReset();
-            document.getElementsByTagName('button')[3].style.color = 'red';
+            buttons[3].style.color = 'red';
             document.body.style.display = 'grid';
             document.body.style.gridTemplateAreas = '"cabecalho cabecalho" "principal lateral"';
             document.querySelector('.cabecalho').style.gridArea = 'header';
@@ -55,7 +56,7 @@ function resposta(resposta) {
     }
 }
 
-function btnReset(){
+function btnReset() {
     const buttons = document.querySelectorAll('button');
 
     for (let i = 0; i < buttons.length && i < 4; i++) {
